5be6c46404be15c49388151d225a9b88a0953b2f
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-01-13 04:24:03 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 // import NewPost from './posts/pages/newPost';
-import Creators from './creator/pages/creators';
+import Creators from './creator/pages/Creators';
 import MainNavigation from '../shared/components/MainNavigation';
 import Homepage from './homepage/pages/Homepage';
- 
+import NewPost from './posts/pages/NewPost';
 import './index.css'
+import Login from './login/Login';
 const App = () => {
 
   return (
@@ -16,7 +17,7 @@ const App = () => {
       <Route path="/" exact><Homepage></Homepage></Route>
       <Route path="/community" exact><Creators /></Route>
       <Route path="/posts/new" exact><NewPost/></Route>
-      
+      <Route path="/login" exact><Login/></Route>
       <Redirect to="/" />
       </Switch>
       </main>
