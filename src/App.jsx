864fe346d64f514e29c6c9a6af7fdabcd57106ee
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-01-13 16:38:03 +0530

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-<<<<<<< HEAD
-// import NewPost from './posts/pages/newPost';
 import Creators from './creator/pages/Creators';
 import MainNavigation from '../shared/components/MainNavigation';
 import Homepage from './homepage/pages/Homepage';
 import NewPost from './posts/pages/NewPost';
-=======
-import NewPost from './posts/pages/newPost';
-import Creators from './creator/pages/creators';
-import MainNavigation from '../shared/components/MainNavigation';
-import Homepage from './homepage/pages/Homepage';
 import UserPosts from './posts/pages/UserPosts';
  
->>>>>>> 75c32866205e3f31fadc5b3ac55cd039a5682879
+// >>>>>>> 75c32866205e3f31fadc5b3ac55cd039a5682879
 import './index.css'
 import Login from './login/Login';
+import ProfilePage from './creator/components/Profile';
 const App = () => {
 
   return (
@@ -30,6 +24,8 @@ const App = () => {
       </Route>
       <Route path="/posts/new" exact><NewPost/></Route>
       <Route path="/login" exact><Login/></Route>
+      <Route path="/profile" exact><ProfilePage/></Route>
+      
       <Redirect to="/" />
       </Switch>
       </main>
